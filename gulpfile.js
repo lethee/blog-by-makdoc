57bf77a39284c659efb23700e1c02ed1f77318e4
Sean Lee <sean@weaveus.com>
2015-02-21 18:52:59 +0900

    update bower and default layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
+var gulp = require('gulp');
 require('gulp-makdoc');
+
+gulp.task('makdoc:done:after', function(done) {
+    gulp.src('bower_components/**/*.{js,css,map}')
+        .pipe(gulp.dest('dist/'));
+   done();
+});
